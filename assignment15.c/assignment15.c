46f226c32171407cbc831fb52be9b38e941a1d84
ahnminjun <brier0504@gmail.com>
2025-04-19 18:43:05 +0900

    Update assignment15.c

--- assignment15.c/assignment15.c
+++ assignment15.c/assignment15.c
@@ -1,12 +1,15 @@
-/* ���ϸ�: assignment15.c
+/* 파일명: assignment15.c
 
- * ����: �������� ���̸� �Է¹޾� ���� ���Ǹ� ���ϴ� ���α׷��� �ۼ��Ͻÿ�.
+ * 내용: 우리가 알고 있는 환율은 매매기준율이다. 실제 달러를 구입할 때는 매매기준율에 은행의 환전수수료를 더해서
+ 환율이 결정된다. 환전수수료는 환전우대율에 따라 달라진다. 환전수수료율은 은행마다 미리 정해지며, 환전우대율은 
+ 고객마다 다르게 책정된다. 환전수수료율은 1.75%로 정해져 있다고 가정하고 원~달러 매매기준율과 환전우대율을 입력받아서
+ 몇 원에 해당하는지 출력하는 프로그램을 작성하시오.
 
- * �ۼ���: �ȹ���
+ * 작성자: 안민준
 
- * ��¥: 2025.04.19.
+ * 날짜: 2025.04.19.
 
- * ����: v1.0
+ * 버전: v1.0
 
  */
 
@@ -14,7 +17,7 @@
 #include <stdio.h>
 
 double assignment15(double base_rate ,double preferential_rate);
-//base_rate�� �Ÿű�����, preferenital_rate�� ȯ�������
+//base_rate는 매매기준율, preferenital_rate는 환율우대율
 
 
 int main()
@@ -25,20 +28,20 @@ int main()
 	double dollar = 0;
 
 
-	printf("��/�޷� �Ÿű�����? ");
+	printf("원/달러 매매기준율? ");
 	scanf("%lf", &base_rate);
 
-	printf("ȯ�������(0~100%)? ");
+	printf("환율우대율(0~100%)? ");
 	scanf("%lf", &preferential_rate);
 
 	double rate = assignment15(base_rate, preferential_rate);
 
-	printf("�޷� �� �� ȯ���� %lf�Դϴ�.\n", rate);
+	printf("달러 살 때 환율은 %lf입니다.\n", rate);
 
-	printf("������ �޷�(USD)? ");
+	printf("구입한 달러(USD)? ");
 	scanf("%lf", &dollar);
 
-	printf("USD %.2lf �� �� ==> KRW %.2lf", dollar, dollar * rate);
+	printf("USD %.2lf 살 때 ==> KRW %.2lf", dollar, dollar * rate);
 
 	return 0;
 
